7bf0bf5d4c78a6e224b5c455a1c7993927429465
JessiiPer <jessica_roch@hotmail.com>
2019-12-17 12:55:19 -0300

    Personalizando o serviço

--- src/br/com/caelum/estoque/modelo/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/modelo/estoque/ws/EstoqueWS.java
@@ -2,20 +2,34 @@ package br.com.caelum.estoque.modelo.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();
 	
-	public List<Item> getItems(){
-		System.out.println("chamando getItems()...");
-		List<Item> lista = dao.todosItens();
-		
-		return lista;
+	@WebMethod(operationName="todosOsItens")
+	@ResponseWrapper(localName="itens")
+	@WebResult(name="item")
+	@RequestWrapper(localName="listaItens")
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+
+	List<Filtro> lista = filtros.getLista();
+	List<Item> result = dao.todosItens(lista);
+
+	return new ListaItens(result);
+
 	}
 }
