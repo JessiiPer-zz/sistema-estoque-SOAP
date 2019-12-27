0c440dbbd4b837ea85cac86e1ae62ee8425b8db9
JessiiPer <jessica_roch@hotmail.com>
2019-12-27 10:57:43 -0300

    Trabalhando com cabeçalhos

--- src/br/com/caelum/estoque/modelo/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/modelo/estoque/ws/EstoqueWS.java
@@ -6,14 +6,15 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.RequestWrapper;
-import javax.xml.ws.ResponseWrapper;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
@@ -21,9 +22,9 @@ public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 	
 	@WebMethod(operationName="todosOsItens")
-	@ResponseWrapper(localName="itens")
+	//@ResponseWrapper(localName="itens")
 	@WebResult(name="item")
-	@RequestWrapper(localName="listaItens")
+	//@RequestWrapper(localName="listaItens")
 	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
 
 	List<Filtro> lista = filtros.getLista();
@@ -31,5 +32,22 @@ public class EstoqueWS {
 
 	return new ListaItens(result);
 
+	}
+	
+	@WebMethod(operationName="CadastrarItem")
+	@WebResult(name="item")
+	public Item cadastrarItem(@WebParam(name="tokenUsuario", header = true) TokenUsuario token,
+			@WebParam(name="item") Item item) throws AutorizacaoException {
+		
+		System.out.println("Cadastrando item: " + item);
+		
+		boolean valido = new TokenDao().ehValido(token);
+		
+		if(!valido) {
+			throw new AutorizacaoException("Falha de autorização");
+		}
+		
+		this.dao.cadastrar(item);
+		return item;
 	}
 }
