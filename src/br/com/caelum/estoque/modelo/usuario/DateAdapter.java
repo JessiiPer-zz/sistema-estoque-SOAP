fa5b4153ad40e00ba2416906b4b5b18cb2a1a6ac
JessiiPer <jessica_roch@hotmail.com>
2019-12-27 12:03:09 -0300

    Tratando dados obrigatórios e formatando data

---
+++ src/br/com/caelum/estoque/modelo/usuario/DateAdapter.java
@@ -0,0 +1,31 @@
+package br.com.caelum.estoque.modelo.usuario;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DateAdapter extends XmlAdapter<String, Date> {
+
+	private String pattern = "dd/MM/yyyy";
+
+    public Date unmarshal(String dateString) throws Exception {
+        return new SimpleDateFormat(pattern).parse(dateString);
+    }
+    
+    public String marshal(Date date) throws Exception {
+    	   return new SimpleDateFormat(pattern).format(date);
+    }
+
+	@Override
+	public Object unmarshal(Object v) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object marshal(Object v) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
